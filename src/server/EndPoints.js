ba0b5db892ac85279e68686ab1128ab1bfde412f
edson <olmedito.90@gmail.com>
2021-03-05 07:12:11 -0400

    diseño de React Native

---
+++ src/server/EndPoints.js
@@ -0,0 +1,4 @@
+export const CREATE_USER = 'http://192.168.0.17:8000/user';
+export const SHOW_USER = 'http://192.168.0.17:8000/user';
+export const SHOW_TAREAS = 'http://192.168.0.17:8000/todo';
+export const CREATE_TAREAS = 'http://192.168.0.17:8000/todo';
